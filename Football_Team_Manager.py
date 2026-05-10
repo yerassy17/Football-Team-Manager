517398da5b5aee862cc7da278459a943bc4166a9
yerassy17 <252558@astanait.edu.kz>
2026-05-10 13:16:32 +0500

    Week2: update Football_Team_Manager.py

--- Football_Team_Manager.py
+++ Football_Team_Manager.py
@@ -1,4 +1,20 @@
 from player import Player
+from team import Team
 
-p1 = Player("Messi", "FW", 90)
-print(p1)
+
+def main():
+    p1 = Player("Messi", "FW", 90)
+    p2 = Player("Ronaldo", "ST", 91)
+    p3 = Player("Mbappe", "LW", 89)
+
+    team = Team("AITU FC")
+
+    team.add_player(p1)
+    team.add_player(p2)
+    team.add_player(p3)
+
+    team.show_players()
+
+
+if __name__ == "__main__":
+    main()
