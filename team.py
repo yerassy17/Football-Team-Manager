dd85bfa9b184482b3116fee6c350187e0f58b8c4
yerassy17 <252558@astanait.edu.kz>
2026-05-15 15:52:11 +0500

    Week2-adding Team class.

--- team.py
+++ team.py
@@ -11,3 +11,14 @@ class Team:
 
         for player in self.players:
             print(player)
+
+    def save_to_file(self):
+        with open("players.txt", "w") as file:
+            for player in self.players:
+                file.write(f"{player.name},{player.position},{player.rating}\n")
+
+    def load_from_file(self):
+        with open("players.txt", "r") as file:
+            for line in file:
+                name, position, rating = line.strip().split(",")
+                print(f"{name} - {position} - {rating}")
